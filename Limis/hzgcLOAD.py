04a59d43c247cbc7c81122938eae4a3ee4a9204e
nightvv <36064784+nightvv@users.noreply.github.com>
2018-04-26 16:25:56 +0800

    Update hzgcLOAD.py

--- Limis/hzgcLOAD.py
+++ Limis/hzgcLOAD.py
@@ -7,7 +7,7 @@ Software: PyCharm
 File: hzgc_load.py
 Time: 2018/4/23 20:12
 """
-
+import re
 from openpyxl import load_workbook
 
 class hzgc_API(object):
@@ -137,7 +137,7 @@ class hzgc_API(object):
         return self.sheet['AD'+str(i+1)].value #抽样基数
 
     def DJ(self,i):
-        return self.sheet['AV'+str(i+1)].value.split('单价：')[-1].split(' ')[0] #单价
+        return self.sheet['AV'+str(i+1)].value.split('单价')[-1].split('留样')[0].replace(':','').replace('；','').replace('：','').split('（')[0] #单价
 
     def ZXBZ(self,i):
         return '/' #执行标准
@@ -171,7 +171,7 @@ class hzgc_API(object):
         return self.sheet['AD'+str(i+1)].value[-1] #检品数量单位
 
     def LYSL(self,i):
-        return self.sheet['AV'+str(i+1)].value.split('留样：')[-1][:-1].split(self.JPSLDW(i))[0] #留样数量
+        return ''.join(re.findall('[0-9]',self.sheet['AV'+str(i+1)].value.split('留样')[-1][:-1].split(' ')[0])) #留样数量
 
     def PZWH(self,i):
         return self.sheet['Y'+str(i+1)].value #批准文号
